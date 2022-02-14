a4f071dd329ffe73aac968b7cd8c4bf16dbdd602
suhyunshim <lisa9963suhyun@gmail.com>
2022-02-14 23:06:08 +0900

    request param

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,5 +1,6 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -73,6 +74,16 @@ public class RequestParamController {
     public String requestParamMap(@RequestParam Map<String, Object> paramMap){
 
         log.info("username={}, age={}",paramMap.get("username"),paramMap.get("age"));
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@RequestParam String username,@RequestParam int age){
+        HelloData helloData = new HelloData();
+        helloData.setUsername(username);
+        helloData.setAge(age);
+
         return "ok";
     }
 }
